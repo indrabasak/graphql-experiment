d2963952abf0be04de013988243792571b875142
Indra Basak <indra.basak@autodesk.com>
2025-03-31 09:51:34 -0700

    added the constants file

---
+++ src/common/constants.ts
@@ -0,0 +1,6 @@
+export const AZURE_URL: string = 'REPLACE_ME';
+export const AZURE_CLIENT_ID: string = 'REPLACE_ME';
+export const AZURE_CLIENT_SECRET: string = 'REPLACE_ME';
+export const AZURE_SCOPE: string = 'REPLACE_ME';
+
+export const OPUS_URL: string = 'REPLACE_ME';
